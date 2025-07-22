7babed7dbc2e39574b1700b37491e9a95d120829
priya-singh27 <priya.beingdev@gmail.com>
2025-07-22 06:13:44 +0530

    array, associative array

--- index.php
+++ index.php
@@ -11,15 +11,36 @@
         <input type="number" name="num1"/><br>
         <label>Enter number:</label><br>
         <input type="number" name="num2"/><br>
-        <input type="submit" value="Submit"/>
+        <input type="submit" name="submit" value="submit"/>
     </form>
 </body>
 </html>
 
 <?php 
-  $num1 = $_POST["num1"];
-  $num2 = $_POST["num2"];
-  $total = $num1 + $num2;
+  $nums= array(1,2,3,4);
+  // foreach($nums as $num){
+  //   echo "The num is: {$num} <br>";
+  // }
 
-  echo "Sum is {$total}"
+  $capitals = array(
+    "USA"=>"Washington D.C.",
+   "Japan"=>"Kyoto", 
+   "India"=>"New Delhi"
+  );
+
+  // foreach($capitals as $key => $value){
+  //   echo"{$key} = {$value} <br>";
+  // }
+
+  echo $capitals["USA"];
+
+  if(isset($_POST["submit"])){
+    $num1=$_POST["num1"];
+    $num2=$_POST["num2"];
+
+    echo"You entered {$num1} and {$num2}<br>";
+    echo"You tried to login";
+  }
+
+  /*  */
 ?>
